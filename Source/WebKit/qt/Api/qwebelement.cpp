d6389d0cc181d6171562ad2cd1fb5642470a87f2
Michał Dutkiewicz <emdeck@gmail.com>
2016-09-29 13:49:08 +0200

    Implement support for Fullscreen API

--- Source/WebKit/qt/Api/qwebelement.cpp
+++ Source/WebKit/qt/Api/qwebelement.cpp
@@ -1502,6 +1502,34 @@ void QWebElement::render(QPainter* painter, const QRect& clip)
     context.restore();
 }
 
+void QWebElement::beginEnterFullScreen()
+{
+#if ENABLE(FULLSCREEN_API)
+    m_element->document().webkitWillEnterFullScreenForElement(m_element);
+#endif
+}
+
+void QWebElement::endEnterFullScreen()
+{
+#if ENABLE(FULLSCREEN_API)
+    m_element->document().webkitDidEnterFullScreenForElement(m_element);
+#endif
+}
+
+void QWebElement::beginExitFullScreen()
+{
+#if ENABLE(FULLSCREEN_API)
+    m_element->document().webkitWillExitFullScreenForElement(m_element);
+#endif
+}
+
+void QWebElement::endExitFullScreen()
+{
+#if ENABLE(FULLSCREEN_API)
+    m_element->document().webkitDidExitFullScreenForElement(m_element);
+#endif
+}
+
 class QWebElementCollectionPrivate : public QSharedData
 {
 public:
