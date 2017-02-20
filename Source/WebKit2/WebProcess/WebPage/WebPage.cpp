7a886181c527dc2bdddd79069e1bf01e03c77bdb
Konstantin Tokarev <annulen@yandex.ru>
2017-02-20 14:59:53 +0300

    Revert "tmp"
    This reverts commit a03d3990b462dcf121cf50693fea92fefae5d4c9.
    
    Change-Id: I81221be00067b2e5d159e566546b59c8f0f9be33

--- Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -243,8 +243,6 @@
 #include <WebKitAdditions/WebPageIncludes.h>
 #endif
 
-#include <QDebug>
-
 using namespace JSC;
 using namespace WebCore;
 
@@ -1157,7 +1155,6 @@ void WebPage::loadURLInFrame(const String& url, uint64_t frameID)
 
 void WebPage::loadRequest(uint64_t navigationID, const ResourceRequest& request, const SandboxExtension::Handle& sandboxExtensionHandle, uint64_t shouldOpenExternalURLsPolicy, const UserData& userData)
 {
-    fprintf(stderr, "%s: url=%s\n", __PRETTY_FUNCTION__, request.url().string().utf8().data());
     SendStopResponsivenessTimer stopper(this);
 
     m_pendingNavigationID = navigationID;
@@ -1673,8 +1670,6 @@ float WebPage::deviceScaleFactor() const
 
 void WebPage::setUseFixedLayout(bool fixed)
 {
-    qDebug() << Q_FUNC_INFO << fixed;
-
     // Do not overwrite current settings if initially setting it to false.
     if (m_useFixedLayout == fixed)
         return;
