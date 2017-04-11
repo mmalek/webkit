f2d88cbfb3917ac8bdfb85a769241489a0b7c5e6
Konstantin Tokarev <annulen@yandex.ru>
2017-04-11 17:11:44 +0300

    Fix MediaPlayerPrivateMediaFoundation::naturalSize
    Change-Id: Iaab5f07ed0e70570f2eb043d048ca3069bff185c

--- Source/WebCore/platform/graphics/win/MediaPlayerPrivateMediaFoundation.cpp
+++ Source/WebCore/platform/graphics/win/MediaPlayerPrivateMediaFoundation.cpp
@@ -174,6 +174,11 @@ MediaPlayer::SupportsType MediaPlayerPrivateMediaFoundation::supportsType(const 
 
 void MediaPlayerPrivateMediaFoundation::load(const String& url)
 {
+    {
+        LockHolder locker(m_cachedNaturalSizeLock);
+        m_cachedNaturalSize = FloatSize();
+    }
+
     startCreateMediaSource(url);
 
     m_networkState = MediaPlayer::Loading;
@@ -216,9 +221,10 @@ bool MediaPlayerPrivateMediaFoundation::supportsFullscreen() const
     return true;
 }
 
-FloatSize MediaPlayerPrivateMediaFoundation::naturalSize() const 
+FloatSize MediaPlayerPrivateMediaFoundation::naturalSize() const
 {
-    return m_size;
+    LockHolder locker(m_cachedNaturalSizeLock);
+    return m_cachedNaturalSize;
 }
 
 bool MediaPlayerPrivateMediaFoundation::hasVideo() const
@@ -748,6 +754,12 @@ void MediaPlayerPrivateMediaFoundation::notifyDeleted()
 
     for (HashSet<MediaPlayerListener*>::const_iterator it = m_listeners.begin(); it != m_listeners.end(); ++it)
         (*it)->onMediaPlayerDeleted();
+}
+
+void MediaPlayerPrivateMediaFoundation::setNaturalSize(const FloatSize& size)
+{
+    LockHolder locker(m_cachedNaturalSizeLock);
+    m_cachedNaturalSize = size;
 }
 
 bool MediaPlayerPrivateMediaFoundation::createOutputNode(COMPtr<IMFStreamDescriptor> sourceSD, COMPtr<IMFTopologyNode>& node)
@@ -1555,6 +1567,25 @@ static bool areMediaTypesEqual(IMFMediaType* type1, IMFMediaType* type2)
     return S_OK == type1->IsEqual(type2, &flags);
 }
 
+static FloatSize calculateNaturalSize(IMFMediaType* mediaType)
+{
+    UINT32 width = 0, height = 0;
+    HRESULT hr = MFGetAttributeSize(mediaType, MF_MT_FRAME_SIZE, &width, &height);
+    if (FAILED(hr) || height == 0)
+        return FloatSize();
+
+    UINT32 parNumerator = 0;
+    UINT32 parDenominator = 0;
+    hr = MFGetAttributeRatio(mediaType, MF_MT_PIXEL_ASPECT_RATIO, &parNumerator, &parDenominator);
+    if (SUCCEEDED(hr) && parNumerator && parDenominator) {
+        FloatSize size(width, height);
+        size.setWidth(size.width() * parNumerator / parDenominator);
+        return size;
+    }
+
+    return FloatSize();
+}
+
 HRESULT MediaPlayerPrivateMediaFoundation::CustomVideoPresenter::setMediaType(IMFMediaType* mediaType)
 {
     if (!mediaType) {
@@ -1611,6 +1642,10 @@ HRESULT MediaPlayerPrivateMediaFoundation::CustomVideoPresenter::setMediaType(IM
         const MFRatio defaultFrameRate = { 30, 1 };
         m_scheduler.setFrameRate(defaultFrameRate);
     }
+
+    // Update natural size
+    if (m_mediaPlayer)
+        m_mediaPlayer->setNaturalSize(calculateNaturalSize(mediaType));
 
     ASSERT(mediaType);
     m_mediaType = mediaType;
